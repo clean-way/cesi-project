851ce82ec4c6a160715506945f4c689f0ada9099
Kevin Morainville <slupshi@gmail.com>
2024-06-24 14:59:21 +0200

    feat: profile page sample

--- src/components/profile/TrashCard.tsx
+++ src/components/profile/TrashCard.tsx
@@ -4,7 +4,7 @@ import { Text } from "@/components/common/display/Texts";
 
 export default function TrashCard({Icon, quantity, label} : {Icon : IconType, quantity: number, label: string}){
     return (
-        <Card className="p-6 space-y-2 flex flex-col justify-center items-center w-[140px]">
+        <Card className="p-4 xl:p-6 space-y-2 flex flex-col justify-center items-center w-[120px] xl:w-[140px]">
             <Icon className="text-primary" size={30}/>
             <Text text={quantity.toString()} variant="h6"/>
             <Text text={label} />
