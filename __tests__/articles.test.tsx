8cf5febe2fad1c1ec44d7b52a51e7a66f6f0cbe7
Kevin Morainville <slupshi@gmail.com>
2024-08-27 14:06:17 +0200

    feat: test of article card

--- __tests__/articles.test.tsx
+++ __tests__/articles.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import ArticlesPage from '@/app/articles/page';
+import '@testing-library/jest-dom'
 import { useSession } from "next-auth/react";
+import ArticleCard from '@/components/profile/ArticleCard';
 jest.mock("next-auth/react");
 
 test('renders articles page with no articles', () => {
@@ -23,4 +25,28 @@ test('renders articles page with no articles', () => {
     const noArticleLabels = screen.getAllByText('Aucun article dans cette catégorie');
     
     expect(noArticleLabels.length).toBe(2);
-  });
+});
+
+test('renders article card', () => {
+
+  const mockedArticle = {
+    id: '1',
+    authorId: '1',
+    body: "article's body",
+    title: "article's title",
+    updatedAt: new Date(),
+    createdAt: new Date(),
+    author: {
+      image: "image",
+      name: "author's name",
+    }
+  };
+
+  render(<ArticleCard id={mockedArticle.id} title={mockedArticle.title} body={mockedArticle.body} date={mockedArticle.createdAt} author={mockedArticle.author}/>);
+
+  const articleBody = screen.getByText(mockedArticle.body);
+  const articleTitle = screen.getByText(mockedArticle.title);
+  
+  expect(articleBody).toBeInTheDocument();
+  expect(articleTitle).toBeInTheDocument();
+});
