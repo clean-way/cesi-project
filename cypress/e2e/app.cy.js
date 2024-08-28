46480feeb2c4576d8485cad83ef7a319db7d3d65
Kevin Morainville <slupshi@gmail.com>
2024-08-28 12:45:05 +0200

    feat: go to map test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,4 +1,4 @@
-describe('Navigation', () => {
+describe('Navigation to articles', () => {
     it('should navigate to the articles page', () => {
         // Start from the index page
         cy.visit('/')
@@ -9,9 +9,29 @@ describe('Navigation', () => {
         // The new url should include "/articles"
         cy.url().should('include', '/articles')
 
-        // The new page should contain an h4 with "Articles du jour"
+        // The new page should contain a div with "Articles du jour"
         cy.get('div').contains('Articles du jour')
 
+        // The new page should contain a div with "Articles de la semaine"
         cy.get('div').contains('Articles de la semaine')
+    })
+})
+
+describe('Navigate to map page', () => {
+    it('should navigate to the sign in page and sign in then go to map', () => {
+        cy.visit('/auth/signin')
+
+        cy.url().should('include', '/auth/signin')
+
+        cy.get('input[name="email"]').type('toto2@gmail.com');
+        cy.get('input[name="password"]').type('1Azertyuiop');
+
+        cy.get('form').submit()
+
+        cy.get('a[href*="map"]').click({multiple: true, force: true})
+
+        cy.url().should('include', '/map')
+
+        cy.get('canvas')
     })
 })
